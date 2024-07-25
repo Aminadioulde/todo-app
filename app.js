6f88c2d3ad2bc766a8b67d8f958a3cdd1936732c
padamGueyeRobotist <pagueye@robotistgroup.com>
2024-07-25 09:42:24 +0000

    TDA-1 : suppression des comments

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-// Get the modal
 var modal = document.getElementById("modal");
 
 
-// Get the <span> element that closes the modal
 var span = document.getElementById("fermer");
 
-// When the user clicks on the button, open the modal
 var btn = document.getElementById("addbtn")
 btn.addEventListener("click",toggleModal);
 function toggleModal(){
@@ -13,12 +10,10 @@ function toggleModal(){
   console.log("amina");
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
